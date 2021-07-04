f94f45d7417d32d8d1d19aae66812433a34c7aa0
Костя Шурыгин <kostiashuryhin@gmail.com>
2021-07-04 17:08:02 +0300

    Start work

---
+++ Home work 15/Home work 15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home work 15
+//
+//  Created by Константин on 4.07.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
