0212c45e2e14dacf6afaecc25f6fd215dd40dc57
Костя Шурыгин <kostiashuryhin@gmail.com>
2021-07-05 12:17:56 +0300

    add aerray

---
+++ Home work 15/Home work 15/Resourses/Constants.swift
@@ -0,0 +1,65 @@
+//
+//  Constants.swift
+//  Home work 15
+//
+//  Created by Константин on 5.07.21.
+//
+
+import Foundation
+struct Person {
+    static var name: String = ""
+    static var surname: String = ""
+    static var phone: String = ""
+    static var email: String = ""
+    
+    static let arrayNames = [
+        "Stuart",
+        "Walter",
+        "Charles",
+        "Michael",
+        "Joshua",
+        "Toby",
+        "John",
+        "Ronald",
+        "Peter",
+        "Frank"
+    ]
+   static let arraySurnames = [
+        "Pearson",
+        "Owens",
+        "Henderson",
+        "Jenkins",
+        "Russell",
+        "Melton",
+        "Parks",
+        "Garrett",
+        "Atkinson",
+        "Phillips"
+    ]
+   static let arrayPhones = [
+        "(8029)987-86-92",
+        "(8029)434-53-76",
+        "(8029)303-75-66",
+        "(8029)040-11-26",
+        "(8029)049-63-14",
+        "(8029)493-86-00",
+        "(8029)047-60-36",
+        "(8029)872-99-90",
+        "(8029)396-71-05",
+        "(8029)984-68-55"
+    ]
+    static let arrayEmails = [
+        "hiyeroucocrau-8298@yopmail.com",
+        "teiffurezeba-6494@yopmail.com",
+        "toixigayoufe-9325@yopmail.com",
+        "broucoddallaha-5072@yopmail.com",
+        "freutreiquacizo-2473@yopmail.com",
+        "wassaveifeuze-9276@yopmail.com",
+        "frepalaupeiza-8271@yopmail.com",
+        "pijeibrukoitti-3869@yopmail.com",
+        "vabavummogroi-5069@yopmail.com",
+        "ratribaprodda-9232@yopmail.com"
+    ]
+}
+
+
